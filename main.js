6a286f04086f43b7e7291a646fc434f1c3d41a0a
William A <125278537+w-alexsander@users.noreply.github.com>
2023-05-15 08:09:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function converterTemperatura() {
     var temperaturaFahrenheit = parseFloat(document.getElementById("inputFahrenheit").value);
+    
+    if (isNaN(temperaturaFahrenheit)) {
+        alert("Por favor, insira um valor numérico válido para a temperatura.");
+        return;
+    }
+    
+    if (temperaturaFahrenheit < -459.67) {
+        alert("A temperatura inserida está abaixo do zero absoluto (-459.67°F).");
+        return;
+    }
+    
     var temperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9;
     document.getElementById("resultadoCelsius").innerHTML = "A temperatura em Celsius é: " + temperaturaCelsius.toFixed(2);
 
